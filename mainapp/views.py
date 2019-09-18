bb135b966f4bcda0dd8fbaa2c25fcb9db283951b
K9173A <K9173A@gmail.com>
2019-09-18 18:24:53 +0300

    AJAX works

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 from .models import Product
 from .forms import ProductForm
@@ -13,17 +14,42 @@ def catalog(request):
     return render(request, 'mainapp/catalog.html', context)
 
 
+def save_good_product(request, form, template):
+    data = {}
+    if request.is_ajax():
+        if form.is_valid():
+            form.save()
+            data = {
+                'form_is_valid': True,
+                'html_product_list': render_to_string(
+                    'mainapp/product_list.html',
+                    context={'products': Product.objects.all()},
+                    request=request
+                )
+            }
+        else:
+            data['form_is_valid'] = False
+
+    data['html_form'] = render_to_string(
+        template,
+        context={'form': form},
+        request=request
+    )
+    return JsonResponse(data)
+
+
 def create_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('main:catalog'))
-    else: # GET
-        form = ProductForm()
-
-    context = {
-        'title': 'Новый продукт',
-        'form': form
-    }
-    return render(request, 'mainapp/create_product.html', context)
+        return save_good_product(request, form, 'mainapp/create_product.html')
+    else:
+        return JsonResponse({
+            'html_form': render_to_string(
+                'mainapp/create_product.html',
+                context={
+                    'form': ProductForm(),
+                    'title': 'Новый продукт'
+                },
+                request=request
+            )
+        })

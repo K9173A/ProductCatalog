4c5bb05b8379865f3d763b0bbe599e5dbb87322e
K9173A <K9173A@gmail.com>
2019-10-15 15:06:29 +0000

    Modify settings

--- ProductCatalog/settings/__init__.py
+++ ProductCatalog/settings/__init__.py
@@ -5,5 +5,5 @@ if os.environ.get('PROD_HEROKU'):
     from .prod_heroku import *
 elif os.environ.get('PROD_LOCAL'):
     from .prod_local import *
-elif os.environ.get('DEV'):
+else:
     from .dev import *

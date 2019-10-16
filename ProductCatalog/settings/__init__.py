413865cc933b41cbe7c1493ce360b0347007603b
K9173A <48845845+K9173A@users.noreply.github.com>
2019-10-17 00:35:34 +0300

    Simplify imports in the settings __init__

--- ProductCatalog/settings/__init__.py
+++ ProductCatalog/settings/__init__.py
@@ -4,8 +4,6 @@ import os
 if os.environ.get('PROD_HEROKU'):
     from .prod_heroku import *
 elif os.environ.get('PROD_LOCAL'):
-    from .base import *
     from .prod_local import *
 else:
-    from .base import *
     from .dev import *
